8067ff821150a8432ef23875d1533dcf86dc3103
yannick forget <yannick forget>
2016-06-03 14:54:01 +0200

    first little bit of design added

---
+++ index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Engelse woorden</title>
+        <!--Import Google Icon Font-->
+        <link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+        <!--Import materialize.css-->
+        <link type="text/css" rel="stylesheet" href="css/materialize.min.css"  media="screen,projection"/>
+        <link type="text/css" rel="stylesheet" href="css/style.css">
+        <!--Let browser know website is optimized for mobile-->
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+        <!--Import jQuery before materialize.js-->
+        <script type="text/javascript" src="js/jquery.js"></script>
+        <script type="text/javascript" src="js/materialize.min.js"></script>
+
+    </head>
+    <body>
+    <nav class="lime darken-1" role="navigation">
+        <div class="nav-wrapper container"><a id="logo-container" class="brand-logo center">Engelse woorden</a>
+            <ul class="right hide-on-med-and-down">
+            </ul>
+            <ul id="nav-mobile" class="side-nav">
+            </ul>
+        </div>
+    </nav>
+
+    <div class="container">
+        <form class="center-block">
+            <div class="row">
+                <div class="input-field">
+                    <input id="icon_prefix"  type="text" class="validate">
+                    <label for="icon_prefix">Antwoord</label>
+                </div>
+            </div>
+        </form>
+    </div>
+
+    </body>
+</html>
